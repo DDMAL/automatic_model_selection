5ed068ebb6ff14410824b4f857ac8f020a55e43b
Francisco J. Castellanos <fcastellanos@dlsi.ua.es>
2021-09-22 13:55:30 +0200

    Creating modules for training the domain classifier

---
+++ train_classifier.py
@@ -0,0 +1,254 @@
+"""Local Fast Trainer
+This is the file for running Calvo Fast Trainer loaclly. Make sure
+to have an 'Images' folder with the correct inputs in the same directory.
+If not, you can change the values in 'inputs' and 'outputs'.
+
+Simply run `python local_fast_trainer.py` to see the output.
+This will call `training_engine_sae.py`.
+
+It should generate 3 files in its current state. A background model,
+a Model 0, and a Log File.
+
+If you're running it in a Rodan container, this will be located in code/Rodan/rodan/jobs/Calvo_classifier
+If the container is already running, try `docker exec -it [container_name] bash` to run the script without
+stopping.
+"""
+
+import logging
+import os
+import sys
+import cv2
+import numpy as np
+import training_engine_classifier as training
+import pdb
+import argparse
+import json
+
+
+# ===========================
+#       CONSTANTS
+# ===========================
+KEY_BACKGROUND_LAYER = "rgba PNG - Layer 0 (Background)"
+KEY_SELECTED_REGIONS = "rgba PNG - Selected regions"
+KEY_RESOURCE_PATH = "resource_path"
+KEY_LAYERS = "layers"
+KEY_IMAGES = "Image"
+
+kPATH_IMAGES_DEFAULT = ["datasets/MS73/images"]
+kPATH_REGION_MASKS_DEFAULT = ["datasets/MS73/regions"]
+kPATH_BACKGROUND_DEFAULT = ["datasets/MS73/layers/background"]
+kPATH_LAYERS_DEFAULT = ["datasets/MS73/layers/staff", "datasets/MS73/layers/neumes"]
+kPATH_OUTPUT_MODELS_DEFAULT = ["Models/MS73/model_background.hdf5", "Models/MS73/model_staff_dom_class.hdf5", "Models/MS73/model_neumes_dom_class.hdf5"]
+kBATCH_SIZE_DEFAULT = 8
+kPATCH_HEIGHT_DEFAULT = 256
+kPATCH_WIDTH_DEFAULT = 256
+kMAX_NUMBER_OF_EPOCHS_DEFAULT = 50
+kNUMBER_SAMPLES_PER_CLASS_DEFAULT = 1000
+kFILE_SELECTION_MODE_DEFAULT = training.FileSelectionMode.SHUFFLE
+kSAMPLE_EXTRACTION_MODE_DEFAULT = training.SampleExtractionMode.RANDOM
+# ===========================
+
+
+def menu():
+    parser = argparse.ArgumentParser(description='Fast trainer')
+
+    parser.add_argument(
+                    '-psr',  
+                    dest='path_src', 
+                    help='List of paths of the source folders that contain the original images.',
+                    action='append'
+                    )
+
+    parser.add_argument(
+                    '-prg',  
+                    dest='path_regions', 
+                    help='Path of the folder that contains the region masks.',
+                    action='append'
+                    )
+
+    parser.add_argument(
+                    '-pbg',  
+                    dest='path_bg', 
+                    help='Path of the folder with the background layer data.',
+                    action='append'
+                    )
+
+    parser.add_argument(
+                    '-pgt',
+                    dest='path_layer', 
+                    help='Paths of the ground-truth folders to be considered (one per layer).', 
+                    action='append'
+                    )
+    
+    parser.add_argument(
+                    '-out',
+                    dest='path_out', 
+                    help='Paths for the models saved after the training.', 
+                    action='append'
+                    )
+
+    parser.add_argument(
+                    '-width',
+                    default=kPATCH_HEIGHT_DEFAULT,
+                    dest='patch_width',
+                    type=int,
+                    help='Patch width'
+                    )
+
+    parser.add_argument(
+                    '-height',
+                    default=kPATCH_WIDTH_DEFAULT,
+                    dest='patch_height',
+                    type=int,
+                    help='Patch height'
+                    )
+
+    parser.add_argument(
+                    '-b',
+                    default=kBATCH_SIZE_DEFAULT,
+                    dest='batch_size',
+                    type=int,
+                    help='Batch size'
+                    )
+
+    parser.add_argument(
+                    '-e',
+                    default=kMAX_NUMBER_OF_EPOCHS_DEFAULT,
+                    dest='max_epochs',
+                    type=int,
+                    help='Maximum number of epochs'
+                    )
+
+    parser.add_argument(
+                    '-n',
+                    default=kNUMBER_SAMPLES_PER_CLASS_DEFAULT,
+                    dest='number_samples_per_class',
+                    type=int,
+                    help='Number of samples per class to be extracted'
+                    )
+
+    parser.add_argument(
+                    '-fm',
+                    default=kFILE_SELECTION_MODE_DEFAULT, 
+                    dest='file_selection_mode',
+                    type=training.FileSelectionMode.from_string, 
+                    choices=list(training.FileSelectionMode), 
+                    help='Mode of selecting images in the training process'
+                    )
+
+    parser.add_argument(
+                    '-sm',
+                    default=kSAMPLE_EXTRACTION_MODE_DEFAULT, 
+                    dest='sample_extraction_mode',
+                    type=training.SampleExtractionMode.from_string, 
+                    choices=list(training.SampleExtractionMode), 
+                    help='Mode of extracing samples for each image in the training process'
+                    )
+
+    args = parser.parse_args()
+
+    args.path_src = args.path_src if args.path_src is not None else kPATH_IMAGES_DEFAULT
+    args.path_regions = args.path_regions if args.path_regions is not None else kPATH_REGION_MASKS_DEFAULT
+    args.path_bg = args.path_bg if args.path_bg is not None else kPATH_BACKGROUND_DEFAULT
+    args.path_layer = args.path_layer if args.path_layer is not None else kPATH_LAYERS_DEFAULT
+    args.path_out = args.path_out if args.path_out is not None else kPATH_OUTPUT_MODELS_DEFAULT
+    
+    print('CONFIG:\n -', str(args).replace('Namespace(','').replace(')','').replace(', ', '\n - '))
+
+    return args
+
+# Return the list of files in folder
+# ext param is optional. For example: 'jpg' or 'jpg|jpeg|bmp|png'
+def list_files(directory, ext=None):
+    list_files =  [os.path.join(directory, f) for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f)) and ( ext==None or re.match('([\w_-]+\.(?:' + ext + '))', f) )]
+
+    return sorted(list_files)
+
+# Return the list of files for each folder in a list of directories.
+# ext param is optional. For example: 'jpg' or 'jpg|jpeg|bmp|png'
+def list_files_per_path(list_folders, ext=None):
+    
+    files = [list_files(path_folder, ext=ext) for path_folder in list_folders]
+    return files
+
+#Initialize the dictionary with the inputs
+def init_input_dictionary(config):
+    inputs = {}
+
+    inputs["Image"] = []
+    inputs[KEY_BACKGROUND_LAYER] = []
+    inputs[KEY_SELECTED_REGIONS] = []
+    inputs[KEY_LAYERS] = []
+    list_src_files = list_files_per_path(config.path_src)
+
+
+    for idx_folder in range(len(list_src_files)):
+
+        path_imgs = list_src_files[idx_folder]
+        parent_path_bg = config.path_bg[idx_folder]
+        parent_path_regions = config.path_regions[idx_folder]
+
+        print (path_imgs)
+        dict_img = {}
+        dict_img[KEY_RESOURCE_PATH] = path_imgs
+        inputs[KEY_IMAGES].append(dict_img)
+
+        path_bgs = [ os.path.join(parent_path_bg, os.path.basename(path_imgs_i)) for path_imgs_i in path_imgs]
+        dict_img = {}
+        dict_img[KEY_RESOURCE_PATH] = path_bgs
+        inputs[KEY_BACKGROUND_LAYER].append(dict_img)
+
+        path_regions = [ os.path.join(parent_path_regions, os.path.splitext(os.path.basename(path_imgs_i))[0] + ".png") for path_imgs_i in path_imgs]
+        dict_img = {}
+        dict_img[KEY_RESOURCE_PATH] = path_regions
+        inputs[KEY_SELECTED_REGIONS].append(dict_img)
+        
+        for path_layer in config.path_layer:
+            path_layers = [ os.path.join(path_layer, os.path.basename(path_imgs_i)) for path_imgs_i in path_imgs]
+            dict_img = {}
+            dict_img[KEY_RESOURCE_PATH] = path_layers
+            inputs[KEY_LAYERS].append(dict_img)
+    
+    return inputs
+
+#Initialize the dictionary with the outputs
+def init_output_dictionary(config):
+    outputs = []
+
+    for path_model in config.path_out:
+        outputs.append(path_model)
+
+    return outputs
+
+
+#########################################################################
+
+config = menu()
+
+# Fail if arbitrary layers are not equal before training occurs.
+
+inputs = init_input_dictionary(config)
+outputs = init_output_dictionary(config)
+
+print(json.dumps(inputs, indent=2))
+print(json.dumps(outputs, indent=2))
+
+num_domains = len(inputs[KEY_IMAGES])
+
+# Call in training function
+status = training.train_domain_classifier(
+    inputs=inputs,
+    num_domains=num_domains,
+    height=config.patch_height,
+    width=config.patch_width,
+    output_path=outputs,
+    file_selection_mode=config.file_selection_mode,
+    sample_extraction_mode=config.sample_extraction_mode,
+    epochs=config.max_epochs,
+    number_samples_per_class=config.number_samples_per_class,
+    batch_size=config.batch_size,
+)
+
+print("Finishing the Fast CM trainer job.")
+
---
+++ training_engine_classifier.py
@@ -0,0 +1,442 @@
+from __future__ import division
+
+import cv2
+import numpy as np
+import random as rd
+import os
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dropout, UpSampling2D, Concatenate
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Input, Masking, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.backend import image_data_format
+#import keras
+import tensorflow as tf
+import threading
+from enum import Enum
+
+kPIXEL_VALUE_FOR_MASKING = -100
+
+class FileSelectionMode(Enum):
+    RANDOM,     \
+    SHUFFLE,    \
+    DEFAULT     \
+    = range(3)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def from_string(s):
+        try:
+            return FileSelectionMode[s]
+        except KeyError:
+            raise ValueError()
+
+class SampleExtractionMode(Enum):
+    RANDOM,     \
+    SEQUENTIAL  \
+    = range(2)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def from_string(s):
+        try:
+            return SampleExtractionMode[s]
+        except KeyError:
+            raise ValueError()
+
+# ===========================
+#       SETTINGS
+# ===========================
+
+# gpu_options = tf.GPUOptions(
+#     allow_growth=True,
+#     per_process_gpu_memory_fraction=0.40
+# )
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+# keras.backend.tensorflow_backend.set_session(sess)
+VALIDATION_SPLIT = 0.2
+# BATCH_SIZE = 16
+# ===========================
+
+
+# ===========================
+#       CONSTANTS
+# ===========================
+KEY_BACKGROUND_LAYER = "rgba PNG - Layer 0 (Background)"
+KEY_SELECTED_REGIONS = "rgba PNG - Selected regions"
+KEY_RESOURCE_PATH = "resource_path"
+KEY_IMAGES = "Image"
+# ===========================
+
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return next(self.it)
+
+
+def get_input_shape(height, width, channels=3):
+    if image_data_format() == "channels_first":
+        return (channels, height, width)
+    else:
+        return (height, width, channels)
+
+
+def get_domain_classifier(num_domains, height, width, pretrained_weights=None):
+    ff = 32
+
+    inputs = Input(shape=get_input_shape(height, width))
+    mask = Masking(mask_value=kPIXEL_VALUE_FOR_MASKING)(inputs)
+
+    conv1 = Conv2D(
+        ff, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(mask)
+    conv1 = Conv2D(
+        ff, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(
+        ff * 2, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(pool1)
+    conv2 = Conv2D(
+        ff * 2, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(
+        ff * 8, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(pool2)
+    conv4 = Conv2D(
+        ff * 8, 3, activation="relu", padding="same", kernel_initializer="he_normal"
+    )(conv3)
+
+    flatten = tf.keras.layers.Flatten() (conv4)
+    dense1 = Dense(16, activation="softmax")(flatten)
+    dense2 = Dense(num_domains, activation="softmax")(dense1)
+
+    model = Model(inputs=inputs, outputs=dense2)
+
+    model.compile(
+        optimizer=Adam(lr=1e-4), loss="binary_crossentropy", metrics=["accuracy"]
+    )
+
+    if pretrained_weights is not None:
+        model.load_weights(pretrained_weights)
+
+    #model.summary()
+    return model
+
+
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe."""
+
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+
+    return g
+
+#Load a Ground-truth image and apply the region mask if it is given
+def load_gt_image(path_file, regions_mask=None):
+    file_obj = cv2.imread(path_file, cv2.IMREAD_UNCHANGED,)  # 4-channel
+    if file_obj is None : 
+        raise Exception(
+            'It is not possible to load the image\n'
+            "Path: " + str(path_file)
+        )
+
+    TRANSPARENCY = 3
+    bg_mask = (file_obj[:, :, TRANSPARENCY] == 255)
+    
+    if regions_mask is not None:
+        masked = np.logical_and(bg_mask, regions_mask) * 1.0
+        l = np.where((regions_mask == 0))
+        masked[l] = kPIXEL_VALUE_FOR_MASKING
+        return masked
+    else:
+        return bg_mask
+
+
+def get_image_with_gt(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file):
+
+    # Required input ports
+    # TODO assert that all layers have the same number of inputs (otherwise it will crack afterwards)
+    number_of_training_pages = len(image_paths)
+    if idx_file >= number_of_training_pages : # If we try to access to an non-existing layer 
+        raise Exception(
+            'The index of the file does not exist\n'
+            "input images: " + str(number_of_training_pages) + " index acceded: " + str(idx_file)
+        )
+
+    gr = (255. - cv2.imread(image_paths[idx_file], cv2.IMREAD_COLOR)) / 255.  # 3-channel
+    gt = labels[idx_file]
+
+    return gr, gt
+
+def appendNewSample(gr, gt, row, col, patch_height, patch_width, gr_chunks, gt_chunks):
+    gr_sample = gr[
+            row : row + patch_height, col : col + patch_width
+        ]  # Greyscale image
+    gr_chunks.append(gr_sample)
+    gt_chunks.append(gt)
+
+
+
+def createGeneratorSingleFileSequentialExtraction(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, row, col, patch_height, patch_width, batch_size):
+    gr, gt = get_image_with_gt(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file)
+
+    gr_chunks = []
+    gt_chunks = []
+
+    hstride = patch_height // 2
+    wstride = patch_width // 2
+    
+    count = 0
+    for r in range(row, gr.shape[0] - patch_height, hstride):
+        for c in range(col, gr.shape[1] - patch_width, wstride):
+            appendNewSample(gr, gt, row, col, patch_height, patch_width, gr_chunks, gt_chunks)
+            count +=1
+            if count % batch_size == 0:
+                gr_chunks_arr = np.array(gr_chunks)
+                gt_chunks_arr = np.array(gt_chunks)
+                # convert gr_chunks and gt_chunks to the numpy arrays that are yield below
+
+                return gr_chunks_arr, gt_chunks_arr, r, c  # convert into npy before yielding
+
+
+
+def extractRandomSamples(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size, sample_extraction_mode):
+    gr, gt = get_image_with_gt(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file)
+
+    gr_chunks = []
+    gt_chunks = []
+
+    for i in range(batch_size):
+        row = np.random.randint(0, gr.shape[0] - patch_height)
+        col = np.random.randint(0, gr.shape[1] - patch_width)
+        appendNewSample(gr, gt, row, col, patch_height, patch_width, gr_chunks, gt_chunks)
+
+    gr_chunks_arr = np.array(gr_chunks)
+    gt_chunks_arr = np.array(gt_chunks)
+    # convert gr_chunks and gt_chunks to the numpy arrays that are yield below
+
+    return gr_chunks_arr, gt_chunks_arr  # convert into npy before yielding
+
+
+def get_stride(patch_height, patch_width):
+    return patch_height // 2, patch_width // 2
+
+@threadsafe_generator  # Credit: https://anandology.com/blog/using-iterators-and-generators/
+def createGeneratorSequentialExtraction(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size):
+    
+    hstride, wstride = get_stride(patch_height, patch_width)
+    
+    gr_chunks = []
+    gt_chunks = []
+
+    gr, gt = get_image_with_gt(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file)
+    count = 0
+    for row in range(0, gr.shape[0] - patch_height, hstride):
+        for col in range(0, gr.shape[1] - patch_width, wstride):
+
+            appendNewSample(gr, gt, row, col, patch_height, patch_width, gr_chunks, gt_chunks)
+            count +=1
+            if count % batch_size == 0:
+                gr_chunks_arr = np.array(gr_chunks)
+                gt_chunks_arr = np.array(gt_chunks)
+                # convert gr_chunks and gt_chunks to the numpy arrays that are yield below
+
+                yield gr_chunks_arr, gt_chunks_arr  # convert into npy before yielding
+                gr_chunks = []
+                gt_chunks = []
+                count = 0
+
+
+@threadsafe_generator  # Credit: https://anandology.com/blog/using-iterators-and-generators/
+def createGeneratorDefault(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode):
+    print("Creating default generator...")
+    
+    number_of_training_pages = len(image_paths)
+
+    while True:
+        for idx_file in range(number_of_training_pages):
+            if sample_extraction_mode == SampleExtractionMode.RANDOM:
+                yield extractRandomSamples(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size, sample_extraction_mode)
+            elif sample_extraction_mode == SampleExtractionMode.SEQUENTIAL:
+                yield createGeneratorSequentialExtraction(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size)
+            else:
+                raise Exception(
+                    'The sample extraction mode does not exist.\n'
+                )
+
+
+@threadsafe_generator  # Credit: https://anandology.com/blog/using-iterators-and-generators/
+def createGeneratorShuffle(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode):
+    print("Creating shuffle generator...")
+    
+    image_paths, labels,
+
+    list_shuffle_idx_files = list(range(len(image_paths)))
+        
+    while True:
+        rd.shuffle(list_shuffle_idx_files)
+        
+        for idx_file in list_shuffle_idx_files:
+            if sample_extraction_mode == SampleExtractionMode.RANDOM:
+                yield extractRandomSamples(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size, sample_extraction_mode)
+            elif sample_extraction_mode == SampleExtractionMode.SEQUENTIAL:
+                yield createGeneratorSequentialExtraction(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size)
+            else:
+                raise Exception(
+                    'The sample extraction mode does not exist.\n'
+                )
+
+@threadsafe_generator  # Credit: https://anandology.com/blog/using-iterators-and-generators/
+def createGeneratorRandom(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode):
+    print("Creating random generator...")
+
+    number_of_training_pages = len(image_paths)
+
+    while True:
+        idx_file = np.random.randint(number_of_training_pages)  # Changed len to grs from gr
+        if sample_extraction_mode == SampleExtractionMode.RANDOM:
+            yield extractRandomSamples(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size, sample_extraction_mode)
+        elif sample_extraction_mode == SampleExtractionMode.SEQUENTIAL:
+            yield createGeneratorSequentialExtraction(inputs, image_paths, labels, region_paths, backgrounds_paths, idx_file, patch_height, patch_width, batch_size)
+        else:
+            raise Exception(
+                'The sample extraction mode does not exist.\n'
+            )
+
+
+
+@threadsafe_generator  # Credit: https://anandology.com/blog/using-iterators-and-generators/
+def createGenerator(inputs, patch_height, patch_width, batch_size, file_selection_mode, sample_extraction_mode):
+    
+    num_domains = len(inputs[KEY_IMAGES])
+    
+    image_paths = [img_path for dict_imgs_folder in inputs[KEY_IMAGES] for img_path in dict_imgs_folder[KEY_RESOURCE_PATH]]
+    region_paths = [img_path for dict_imgs_folder in inputs[KEY_SELECTED_REGIONS] for img_path in dict_imgs_folder[KEY_RESOURCE_PATH]]
+    backgrounds_paths = [img_path for dict_imgs_folder in inputs[KEY_BACKGROUND_LAYER] for img_path in dict_imgs_folder[KEY_RESOURCE_PATH]]
+    labels = [idx_folder for idx_folder in range(len(inputs[KEY_IMAGES])) for img_path in inputs[KEY_IMAGES][idx_folder][KEY_RESOURCE_PATH]]
+    labels = np.eye(num_domains)[labels]
+    
+    if file_selection_mode == FileSelectionMode.DEFAULT:
+        return createGeneratorDefault(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode)
+    elif file_selection_mode == FileSelectionMode.SHUFFLE:
+        return createGeneratorShuffle(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode)
+    elif file_selection_mode == FileSelectionMode.RANDOM:
+        return createGeneratorRandom(inputs, image_paths, labels, region_paths, backgrounds_paths, patch_height, patch_width, batch_size, sample_extraction_mode)
+    else:
+        raise Exception(
+            'The file extraction mode does not exist.\n'
+        ) 
+
+
+
+def getTrain(inputs, num_labels, patch_height, patch_width, batch_size, file_selection_mode, sample_extraction_mode):
+    generator_labels = []
+
+    print("num_labels", num_labels)
+    for idx_label in range(num_labels):
+        print("idx_label", idx_label)
+        generator_label = createGenerator(
+            inputs, patch_height, patch_width, batch_size, file_selection_mode, sample_extraction_mode
+        )
+        generator_labels.append(generator_label)
+        print(generator_labels)
+
+    return generator_labels
+
+
+
+def get_number_samples_sequential(inputs, patch_height, patch_width):
+    hstride, wstride = get_stride(patch_height, patch_width)
+    number_samples = 0
+
+    for idx_file in range(len(inputs["Image"])):
+        gr = cv2.imread(inputs["Image"][idx_file][KEY_RESOURCE_PATH], cv2.IMREAD_COLOR)  # 3-channel
+        number_samples += ((gr.shape[0] - patch_height) // hstride) * ((gr.shape[1] - patch_width) // wstride)
+
+    return number_samples
+
+def get_steps_per_epoch(inputs, number_samples_per_class, patch_height, patch_width, batch_size, sample_extraction_mode):
+
+    if sample_extraction_mode == SampleExtractionMode.RANDOM:
+        return number_samples_per_class // batch_size
+    elif sample_extraction_mode == SampleExtractionMode.SEQUENTIAL:
+        return get_number_samples_sequential(inputs, patch_height, patch_width)
+    else:
+        raise Exception(
+            'The sample extraction mode does not exist.\n'
+        )
+        
+    
+
+def train_domain_classifier(
+    inputs,
+    num_domains,
+    height,
+    width,
+    output_path,
+    file_selection_mode,
+    sample_extraction_mode,
+    epochs,
+    number_samples_per_class,
+    batch_size=16,
+):
+
+    # Create ground_truth
+    print("Creating data generators...")
+    generators = getTrain(inputs, num_domains, height, width, batch_size, file_selection_mode, sample_extraction_mode)
+    generators_validation = getTrain(inputs, num_domains, height, width, batch_size, FileSelectionMode.DEFAULT, SampleExtractionMode.RANDOM)
+
+    # Training loop
+    for label in range(num_domains):
+        print("Training a new model for label #{}".format(str(label)))
+        model = get_domain_classifier(num_domains=len(inputs[KEY_IMAGES]), height=height, width=width)
+        # model.summary()
+
+        callbacks_list = [
+            ModelCheckpoint(
+                output_path[label],
+                save_best_only=True,
+                monitor="val_accuracy",
+                verbose=1,
+                mode="max",
+            ),
+            EarlyStopping(monitor="val_accuracy", patience=3, verbose=0, mode="max"),
+        ]
+
+        steps_per_epoch = get_steps_per_epoch(inputs, number_samples_per_class, height, width, batch_size, sample_extraction_mode)
+
+        # Training stage
+        model.fit(
+            generators[label],
+            verbose=2,
+            steps_per_epoch=steps_per_epoch,
+            validation_data=generators_validation[label],
+            validation_steps=100,
+            callbacks=callbacks_list,
+            epochs=epochs,
+        )
+
+    return 0
+
+
+# Debugging code
+if __name__ == "__main__":
+    print("Must be run from Rodan")
